ae43314cee358e2e44172eedd4c1c2879e48d40c
fangyunlong <fangyunlong@qianshengqian.com>
2018-03-30 11:48:43 +0800

    增加修改

--- build/utils.js
+++ build/utils.js
@@ -1,14 +1,19 @@
 'use strict'
+// 略
 const path = require('path')
+// 引入config/index.js(就是vue-cli的配置文件)
 const config = require('../config')
+// 用于抽离css样式，防止将样式打包在js中引起的样式错乱
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
+// 获取packa.json
 const packageConfig = require('../package.json')
 
+// 处理静态资源路径？
 exports.assetsPath = function (_path) {
   const assetsSubDirectory = process.env.NODE_ENV === 'production'
     ? config.build.assetsSubDirectory
     : config.dev.assetsSubDirectory
-
+// 生成静态资源文件夹下的路径 根据process.env.NODE_ENV来区分路径
   return path.posix.join(assetsSubDirectory, _path)
 }
 
@@ -50,11 +55,16 @@ exports.cssLoaders = function (options) {
         fallback: 'vue-style-loader'
       })
     } else {
+      // 这里因为loaders是从右往左处理，所以直接从concat即可，最终解析样式生成style标签，用的是vue-style-loader 
       return ['vue-style-loader'].concat(loaders)
     }
   }
 
   // https://vue-loader.vuejs.org/en/configurations/extract-css.html
+
+  // 1.JS文件无非就是做babel处理，所以无需在utils中额外处理
+  // 2.样式文件包含多种情况，有css、less、sass、scss、stylus、postcss所以在utils中预先处理了
+  // 3.这里最终返回的是对应哪些文件具体要用哪些loader
   return {
     css: generateLoaders(),
     postcss: generateLoaders(),
@@ -67,6 +77,8 @@ exports.cssLoaders = function (options) {
 }
 
 // Generate loaders for standalone style files (outside of .vue)
+
+// 1.这里就直接生成了对应module.rules下的东西，可以直接引用
 exports.styleLoaders = function (options) {
   const output = []
   const loaders = exports.cssLoaders(options)
@@ -81,7 +93,7 @@ exports.styleLoaders = function (options) {
 
   return output
 }
-
+// 编译出错时，弹窗提示。原来是这个鬼（编译出错提示）
 exports.createNotifierCallback = () => {
   const notifier = require('node-notifier')
 
